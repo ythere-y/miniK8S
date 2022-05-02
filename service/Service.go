6bfaf92a7dbda69aaf53d3e1a51357e329ba2e32
ythere-y <1069043992@qq.com>
2022-05-02 12:23:18 +0800

    service类放置

--- service/Service.go
+++ service/Service.go
@@ -1,6 +1,12 @@
 package service
 
+import (
+	"minik8s/apimachinery/pkg/apis/meta"
+)
+
 type Service struct {
-	name  string
-	_type string
+	meta.TypeMeta   `json:",inline" :"meta_._type_meta"`
+	meta.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata" :"meta_._object_meta"`
+	Spec            meta.ServiceSpec   `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec" :"spec"`
+	Status          meta.ServiceStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status" :"status"`
 }
