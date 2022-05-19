1dddeeefaedc219a5251470fa72112d91824878c
ythere-y <1069043992@qq.com>
2022-05-19 19:26:10 +0800

    完成etcd的前缀数量查询、存在查询两个功能函数（可调用apiserver）

--- apiserver/commands.go
+++ apiserver/commands.go
@@ -3,6 +3,7 @@ package apiserver
 import (
 	"encoding/json"
 	"fmt"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"io/ioutil"
 	"minik8s/constant"
 	"minik8s/lab/etcd"
@@ -50,7 +51,7 @@ func PushPodToScheduler(pod pod.Pod) error {
 func DistributePodtoNode(nodeName string, podName string) error {
 	key := etcd.SetKey(
 		etcd.SetPrefix(constant.RegistryPrefix),
-		etcd.SetSourceType("pods"),
+		etcd.SetSourceType("nodes"),
 		etcd.SetNameSpace("default"),
 		etcd.SetNodeName(nodeName),
 		etcd.SetPodName(podName))
@@ -61,6 +62,11 @@ func DistributePodtoNode(nodeName string, podName string) error {
 	return nil
 }
 
+//GetPodInfo
+/*
+ 根据podName，从/registry/pods/目录下寻找对应的pod
+并组建成Pod返回
+*/
 func GetPodInfo(podName string) pod.Pod {
 	getRes, err := etcd.Get(etcd.SetKey(
 		etcd.SetPrefix(constant.RegistryPrefix),
@@ -74,4 +80,39 @@ func GetPodInfo(podName string) pod.Pod {
 	utils.HandleError("unmarshal pod failed ", err)
 
 	return podInfo
+}
+
+//GetChildNum
+/*
+根据key值，使用前缀查找，统计查询到的结果数量并返回
+*/
+func GetChildNum(key string) uint32 {
+	var (
+		num    uint32 = 0
+		getRsp *clientv3.GetResponse
+		err    error
+	)
+	num = 1
+	getRsp, err = etcd.GetWithPrefix(key)
+	utils.HandleError("get with prefix error[from get child num]", err)
+	num = uint32(len(getRsp.Kvs))
+
+	return num
+}
+
+//CheckIfExist
+/*
+根据key值，使用准确查找，检查某个key值是否存在
+*/
+func CheckIfExist(key string) bool {
+	var (
+		exist  bool = false
+		getRsp *clientv3.GetResponse
+		err    error
+	)
+	getRsp, err = etcd.GetNormal(key)
+	utils.HandleError("get with prefix error[from get child num]", err)
+	exist = len(getRsp.Kvs) == 1
+	return exist
+
 }
--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"minik8s/cmd"
-	"minik8s/controllerManager"
-	"minik8s/kubelet"
-	"minik8s/scheduler"
+	"minik8s/apiserver"
 	"time"
 )
 
@@ -42,14 +39,15 @@ var rootName string
 
 func main() {
 	//go environment.Main() //测试启动前的环境准备
-	go controllerManager.Main() // 启动controller manager
-	go scheduler.Main()         // 启动scheduler
-	go kubelet.Main()           // 启动kubelet
-	//apiserver.ApiServerMain() //关于apiserver的测试
-	//etcd.LabMain() //关于etcd的测试
+	//go controllerManager.Main() // 启动controller manager
+	//go scheduler.Main()         // 启动scheduler
+	//go kubelet.Main()           // 启动kubelet
+	apiserver.ApiServerTest() //关于apiserver的测试
+	//go etcd.LabMain() //关于etcd的测试
+
 	time.Sleep(2 * time.Second)
 
-	cmd.RootCmdRun() // 关于命令行的测试
+	//cmd.RootCmdRun() // 关于命令行的测试
 	//service.OutPutFmtTest() //关于格式化输出的测试
 	//service.SerReadTest() // 关于读取yamle文件建立service的测试
 
