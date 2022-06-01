d6211d5fa9c1828cbe15f4c425812fe735b127b4
ythere-y <1069043992@qq.com>
2022-06-01 23:24:46 +0800

    first_try

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
-import "minik8s/shellScripts"
+import "minik8s/lab/rootContainer"
 
 var rootName string
 
 func main() {
 	//log.Println("hello world")
-	shellScripts.Main()
+	//shellScripts.Main()
+	rootContainer.Test()
 	//go kubernetes.Main() // kubernetes的测试部分
 	//go cni.Main() // 测试CNI插件功能部分
 	//go environment.Main() //测试启动前的环境准备
