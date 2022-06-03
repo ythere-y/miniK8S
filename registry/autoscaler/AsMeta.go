cdc2b9c4c7d0cee2f308d64a2c82114b41279c7c
spikexyx <772978056@qq.com>
2022-06-04 04:11:44 +0800

    add autoscaler basic part

---
+++ registry/autoscaler/AsMeta.go
@@ -0,0 +1,39 @@
+package autoscaler
+
+type AutoScalerMetrics struct {
+	Cpu float32
+	Mem float32
+}
+
+type AutoScalerSpec struct {
+	MinReplicas uint32
+	MaxReplicas uint32
+	Metrics     AutoScalerMetrics
+	Selector    struct {
+		Labels map[string]string
+	}
+}
+
+type AutoScaler struct {
+	Kind     string
+	Name     string
+	Workload string
+	Spec     AutoScalerSpec
+}
+
+type AutoScalerYaml struct {
+	Kind     string `yaml:"kind"`
+	Name     string `yaml:"name"`
+	Workload string `yaml:"workload"`
+	Spec     struct {
+		MinReplicas uint32 `yaml:"minreplicas"`
+		MaxReplicas uint32 `yaml:"maxreplicas"`
+		Metrics     struct {
+			Cpu float32 `yaml:"cpu"`
+			Mem float32 `yaml:"memory"`
+		}
+		Selector struct {
+			Labels map[string]string `yaml:"labels"`
+		}
+	}
+}
---
+++ registry/autoscaler/AutoScaler.go
@@ -0,0 +1,32 @@
+package autoscaler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+func ParseAutoScaler(file string) AutoScaler {
+	var asYaml AutoScalerYaml
+	yamlFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("autoscaler yaml file parse err.")
+	}
+
+	err = json.Unmarshal(yamlFile, &asYaml)
+	if err != nil {
+		fmt.Println("unmarshal autoscaler error.")
+	}
+
+	var newAutoScaler AutoScaler
+	newAutoScaler.Kind = asYaml.Kind
+	newAutoScaler.Name = asYaml.Name
+	newAutoScaler.Workload = asYaml.Workload
+	newAutoScaler.Spec.MinReplicas = asYaml.Spec.MinReplicas
+	newAutoScaler.Spec.MaxReplicas = asYaml.Spec.MaxReplicas
+	newAutoScaler.Spec.Metrics.Cpu = asYaml.Spec.Metrics.Cpu
+	newAutoScaler.Spec.Metrics.Mem = asYaml.Spec.Metrics.Mem
+	newAutoScaler.Spec.Selector.Labels = asYaml.Spec.Selector.Labels
+
+	return newAutoScaler
+}
