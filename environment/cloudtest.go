b6bd6a7a2988f096c16dd9c9d85839f70e9c4109
ythere-y <1069043992@qq.com>
2022-06-03 21:26:03 +0800

    增加更多打印

--- environment/cloudtest.go
+++ environment/cloudtest.go
@@ -52,6 +52,7 @@ func startupEtcd(thisIP string) {
 	totalString += cmdLine + "\n"
 
 	// 写入脚本
+	fmt.Printf("ready to write [file = %v], context ->:\n%v\n", constant.EtcdSh, totalString)
 	script.Echo(totalString).WriteFile(constant.EtcdSh)
 
 	// 检验写入结果
