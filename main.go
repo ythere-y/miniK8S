bd9805e744566d7a75d0a2d65b52103501efcad8
ythere-y <1069043992@qq.com>
2022-06-03 17:49:28 +0800

    解决pod下的残留数据

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"minik8s/apiserver"
-	"minik8s/cmd"
+	"minik8s/kubernetes"
 	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -44,8 +44,16 @@ func watchwithTimeTest() {
 	time.Sleep(2 * time.Second)
 	apiserver.SyncPut("/test", "test2")
 }
+
+func Debug() {
+
+	kubernetes.StartUpMaster()
+
+	apiserver.DisplayAllPodsInfo()
+}
 func main() {
 	//log.Println("hello world")
+	Debug()
 	//shellScripts.Main()
 	//rootContainer.Test()
 	//watchwithTimeTest() // 关于定时watch的测试
@@ -61,7 +69,7 @@ func main() {
 	//config.Main()
 	//time.Sleep(2 * time.Second)
 	//go kubernetes.StartUpMaster()	// master节点的初始化startup
-	cmd.RootCmdRun() // 关于命令行的测试
+	//cmd.RootCmdRun() // 关于命令行的测试
 	//service.OutPutFmtTest() //关于格式化输出的测试
 	//service.SerReadTest() // 关于读取yamle文件建立service的测试
 
