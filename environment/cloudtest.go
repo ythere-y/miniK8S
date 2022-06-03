a1ab544bd54d39e814c6cbda30062342f0be4bf5
ythere-y <1069043992@qq.com>
2022-06-03 22:19:22 +0800

    在etcd和flannel之间增加sleep

--- environment/cloudtest.go
+++ environment/cloudtest.go
@@ -6,6 +6,7 @@ import (
 	"minik8s/constant"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func check(name string) string {
@@ -44,12 +45,6 @@ func startupEtcd(thisIP string) {
 	if err != nil {
 		panic(err)
 	}
-	// 检查etcd运行状态
-	cmdLine = "etcdctl  --endpoints http://" + thisIP + ":2379 member list\n"
-	totalString += cmdLine + " \n"
-	// 设置flannel参数
-	cmdLine = "etcdctl set  /coreos.com/network/config '{\"Network\": \"10.0.0.0/16\", \"SubnetLen\": 24, \"SubnetMin\": \"10.0.10.0\",\"SubnetMax\": \"10.0.20.0\", \"Backend\": {\"Type\": \"vxlan\"}}'"
-	totalString += cmdLine + " \n"
 
 	// 写入脚本
 	//fmt.Printf("ready to write [file = %v], context ->:\n%v\n", constant.EtcdSh, totalString)
@@ -98,6 +93,13 @@ func startupFlannel(targetIP string) {
 		get         string
 		err         error
 	)
+	// 检查etcd运行状态
+	cmdLine = "etcdctl  --endpoints http://" + targetIP + ":2379 member list\n"
+	totalString += cmdLine + " \n"
+	// 设置flannel参数
+	cmdLine = "etcdctl set  /coreos.com/network/config '{\"Network\": \"10.0.0.0/16\", \"SubnetLen\": 24, \"SubnetMin\": \"10.0.10.0\",\"SubnetMax\": \"10.0.20.0\", \"Backend\": {\"Type\": \"vxlan\"}}'"
+	totalString += cmdLine + " \n"
+
 	// 启动flannel
 	cmdLine = "flannel -etcd-endpoints \"http://" + targetIP + ":4001,http://" + targetIP + "192.168.1.13:2379\""
 	totalString += cmdLine + " &\n"
@@ -136,5 +138,6 @@ func startupFlannel(targetIP string) {
 func main() {
 	fmt.Print("hello world\n")
 	startupEtcd("192.168.1.4")
+	time.Sleep(5 * time.Second)
 	startupFlannel("192.168.1.4")
 }
