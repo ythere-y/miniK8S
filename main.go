2d4be613cdb48b99b60e3564cde34135d815a7c1
spikexyx <772978056@qq.com>
2022-06-03 16:52:31 +0800

    test

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"minik8s/apiserver"
 	"minik8s/cmd"
 	"minik8s/controllerManager"
-	"minik8s/kubelet"
 	"minik8s/kubernetes"
+	"minik8s/utils"
 	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -57,9 +57,9 @@ func main() {
 	time.Sleep(time.Second * 3)
 	//go cni.Main() // 测试CNI插件功能部分
 	//go environment.Main() //测试启动前的环境准备
-	go controllerManager.Main() // 启动controller manager
+	// go controllerManager.Main() // 启动controller manager
 	//go scheduler.Main()         // 启动scheduler
-	go kubelet.Main() // 启动kubelet
+	// go kubelet.Main() // 启动kubelet
 	//apiserver.ApiServerTest() //关于apiserver的测试
 	//go etcd.LabMain() //关于etcd的测试
 	//config.Main()
@@ -70,7 +70,7 @@ func main() {
 	//service.SerReadTest() // 关于读取yamle文件建立service的测试
 
 	//circle.CircleTest() // 关于循环import的测试
-	//utils.HoldPro() // 阻塞进程防止运行结束
+	utils.HoldPro() // 阻塞进程防止运行结束
 
 	return
 
