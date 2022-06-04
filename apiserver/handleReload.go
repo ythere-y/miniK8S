a0429eb41aca2bed6ccd5274a9d45e47dc54a5f8
spikexyx <772978056@qq.com>
2022-06-04 15:11:50 +0800

    fix bug

--- apiserver/handleReload.go
+++ apiserver/handleReload.go
@@ -3,12 +3,14 @@ package apiserver
 import (
 	"minik8s/constant"
 	"minik8s/lab/etcd"
+	"time"
 )
 
 func Reload() {
 	key := etcd.SetKey(
 		etcd.SetPrefix(constant.ControllerPrefix),
 		etcd.JustAppend(constant.RELOAD),
+		etcd.JustAppend(time.Now().String()),
 	)
 	value := "reload"
 	etcd.Put(key, string(value))
